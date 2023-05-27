d8425ebde56461cedbab5b5ac5564c6384076b31
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-27 03:45:10 -0400

    add ability for administrators to see a list of all categories and create new categories

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
 
   get '/about', to: 'pages#about'
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
