2ab5ea67a94ec4f69a72d9b74653ef233f39ae4a
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-27 13:57:42 -0400

    Add basic error handling to login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
+    flash.now[:alert] = 'Invalid email or password'
 
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
